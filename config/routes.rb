da78f61b54dd68b31e8405d019e1710754670f0a
JosephLouie <joseph161216@gmail.com>
2021-10-02 23:28:09 -0700

    Add auth auth0 to app

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   resources :tickets
   resources :comments
   resources :project_histories
+  get '/api/public', to: 'public#public'
+  get '/api/private', to: 'private#private'
+  get '/api/private-scoped', to: 'private#private_scoped'
 end
